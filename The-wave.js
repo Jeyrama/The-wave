33005cf6b6367d03ddfc2fccba16029c83948c1d
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-15 16:46:49 -0700

    Update The-wave.js

--- The-wave.js
+++ The-wave.js
@@ -35,3 +35,15 @@ function wave(str) {
 }
 
 // or
+
+function wave(str) {
+  let newArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let copy = str.split('');
+    if (copy[i] !== ' ') {
+      copy[i] = copy[i].toUpperCase()
+      newArr.push(copy.join(''))
+    }
+  }
+  return newArr
+}
