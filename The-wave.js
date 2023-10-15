3e408314b1ef93675f4ce0262e26c33e2c3d63cc
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-15 16:38:00 -0700

    Update The-wave.js

--- The-wave.js
+++ The-wave.js
@@ -23,3 +23,15 @@ Example:
 
 
 // Solution
+
+function wave(str) {
+  let result = [];
+  str.split("").forEach((char, index) => {
+    if (/[a-z]/.test(char)) {
+      result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
+    }
+  });
+  return result;
+}
+
+// or
